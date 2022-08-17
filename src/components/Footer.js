99924767db24a6b57c4a80c7d3982c52dd87a45a
benyoko71 <64444014+benyoko71@users.noreply.github.com>
2022-08-17 00:02:43 -0700

    sample site commit

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Button } from './Button'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className='footer-subscription'>
+                <p className='footer-subscription-heading'>
+                    Join the Adventure newsletter to recieve our best vasation deals
+                </p>
+                <p className='footer-subscription-text'>
+                    You can unsubscribe at anytime.
+                </p>
+                <div className='input-areas'>
+                    <form>
+                        <input
+                            type='email'
+                            name='email'
+                            placeholder='Your Email'
+                            className='footer-input' />
+                        <Button buttonStyle='btn--outline'>Subscribe</Button>
+                    </form>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import '../../App.css';
+
+
+export default function SignUp() {
+    return <h1 className='sign-up'>SIGN UP</h1>;
+}
